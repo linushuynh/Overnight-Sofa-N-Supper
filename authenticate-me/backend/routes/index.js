5e155210640d2dd75e0e05d33e6452fb3eb19396
linushuynh <linusmhuynh@gmail.com>
2022-10-19 14:50:30 -0700

    API router added

--- authenticate-me/backend/routes/index.js
+++ authenticate-me/backend/routes/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api');
 
 // router.get('/hello/world', function(req, res) {
 //   res.cookie('XSRF-TOKEN', req.csrfToken());
 //   res.send('Hello World!');
 // });
 
+router.use('/api', apiRouter);
+
+// THIS IS TO RESTORE THE CSRF TOKEN
 router.get("/api/csrf/restore", (req, res) => {
     const csrfToken = req.csrfToken();
     res.cookie("XSRF-TOKEN", csrfToken);
