cf055264bad13b7c6717629fd3ba735ac315f63b
linushuynh <linusmhuynh@gmail.com>
2022-10-21 15:02:40 -0700

    firstName and lastName attributes added to User

---
+++ authenticate-me/backend/db/migrations/20221021213857-add-user-cols.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addColumn('Users', 'firstName', {
+      type: Sequelize.STRING,
+      allowNull: false
+    });
+    await queryInterface.addColumn('Users', 'lastName', {
+      type: Sequelize.STRING,
+      allowNull: false
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.removeColumn('Users', 'firstName');
+    await queryInterface.removeColumn('Users', 'lastName');
+  }
+};
