29c9f4d48ee31ed99e372df7af75fa3a3b566149
linushuynh <linusmhuynh@gmail.com>
2022-10-27 16:35:07 -0700

    delete review-images and spot-images complete

--- backend/routes/api/review-images.js
+++ backend/routes/api/review-images.js
@@ -1,10 +1,45 @@
 const express = require('express');
 const router = express.Router();
 
-const { ReviewImage } = require('../../db/models');
+const { Review, ReviewImage } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 const { requireAuth } = require('../../utils/auth');
 
+router.delete(
+    '/:imageId',
+    requireAuth,
+    async (req, res) => {
+        const { user } = req;
+        const { imageId } = req.params;
+
+        const deleteReviewImg = await ReviewImage.findByPk(imageId);
+
+        // Error handling for non-existent review
+        if (!deleteReviewImg) {
+            res.status(404)
+            return res.json({
+                message: "Review Image couldn't be found",
+                statusCode: 404
+            })
+        }
+
+        // Authorization for owners only
+        let review = await Review.findByPk(deleteReviewImg.reviewId);
+        review = review.toJSON();
+        if (review.userId !== user.id) {
+            res.status(403);
+            return res.json({
+                message: 'You are unauthorized to delete this image'
+            })
+        }
+
+        await deleteReviewImg.destroy();
+        return res.json({
+            message: "Succesfully deleted",
+            statusCode: 200
+        })
+    }
+);
 
 module.exports = router;
