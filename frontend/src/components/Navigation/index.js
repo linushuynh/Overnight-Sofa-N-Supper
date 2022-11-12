a4a9b094ab1b5612e9cf23714d371165d23d944f
linushuynh <linusmhuynh@gmail.com>
2022-11-11 16:42:15 -0800

    Login form modal created

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -2,33 +2,34 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
-import './Navigation.css'
+import LoginFormModal from '../LoginFormModal';
+import './Navigation.css';
 
-const Navigation = ({ isLoaded }) => {
-    const sessionUser = useSelector(state => state.session.user);
+function Navigation({ isLoaded }){
+  const sessionUser = useSelector(state => state.session.user);
 
-    let sessionLinks;
-    if (sessionUser) {
-      sessionLinks = (
-        <ProfileButton user={sessionUser} />
-      );
-    } else {
-      sessionLinks = (
-        <>
-          <NavLink to="/login">Log In</NavLink>
-          <NavLink to="/signup">Sign Up</NavLink>
-        </>
-      );
-    }
-
-    return (
-      <ul>
-        <li>
-          <NavLink exact to="/">Home</NavLink>
-          {isLoaded && sessionLinks}
-        </li>
-      </ul>
+  let sessionLinks;
+  if (sessionUser) {
+    sessionLinks = (
+      <ProfileButton user={sessionUser} />
     );
+  } else {
+    sessionLinks = (
+      <>
+        <LoginFormModal />
+        <NavLink to="/signup">Sign Up</NavLink>
+      </>
+    );
+  }
+
+  return (
+    <ul>
+      <li>
+        <NavLink exact to="/">Home</NavLink>
+        {isLoaded && sessionLinks}
+      </li>
+    </ul>
+  );
 }
 
 export default Navigation;
