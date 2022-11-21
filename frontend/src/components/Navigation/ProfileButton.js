7abe0ae2a7d82ee6cc425e8b56536bda9f20f0ad
linushuynh <linusmhuynh@gmail.com>
2022-11-20 23:51:36 -0800

    logged-in dropdown styled

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,13 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect} from "react";
 import { useDispatch } from 'react-redux';
 import * as sessionActions from '../../store/session';
 import './Navigation.css';
-import { useHistory} from "react-router-dom";
+import { useLocation, useHistory} from "react-router-dom";
 
 const ProfileButton = ({ user, setShowModal, setLogin }) => {
     const dispatch = useDispatch();
     const [showMenu, setShowMenu] = useState(false);
     const history = useHistory();
+    const location = useLocation();
 
     const openMenu = () => {
       if (showMenu) return;
@@ -30,9 +31,8 @@ const ProfileButton = ({ user, setShowModal, setLogin }) => {
       e.preventDefault();
       dispatch(sessionActions.logout())
       .then(() => {
-
+        if (location.pathname === "/hosting") history.push('/')
       })
-      ;
     };
 
     return (
@@ -51,22 +51,31 @@ const ProfileButton = ({ user, setShowModal, setLogin }) => {
         </button>
         )}
         {showMenu && (user ?
-            <ul className="profile-dropdown" id="logged-in-list">
-             <div className="button-holder " id="logged-in-dropdown">
-              <li className="dropdown-text user-text">{user.username}</li>
-              <li className="dropdown-text user-text">{user.email}</li>
+          <div id="logged-in-dropdown">
+            <div className="profile-dropdown">
+             <div className="login-dropdown-item">
+                <div className="login-item">{user.username}</div>
+             </div>
+             <div className="login-dropdown-item">
+              <div className="login-item">{user.email}</div>
+             </div>
+             <div id="login-linebreak">
               <hr />
-                <button id="login-button manage-listings" className="form-button" onClick={() => history.push('/hosting')}>
+             </div>
+              <div className="login-dropdown-item" id="manage-listings" onClick={() => history.push('/hosting')}>
+                <div className="login-item">
                   Manage listings
-                </button>
-
-              <li id="logout-box">
-                <button id="login-button" className="form-button logout-box" onClick={logout}>Log Out</button>
-              </li>
+                </div>
+              </div>
+              <div className="login-dropdown-item" id="logout-button" onClick={logout}>
+                <div id="logout-box">
+                  <div  className="login-item" >Log Out</div>
+                </div>
+              </div>
+            </div>
           </div>
-            </ul>
           :
-          <ul className="profile-dropdown">
+          <div className="profile-dropdown">
         <div className="button-holder">
           <li >
             <button
@@ -86,20 +95,8 @@ const ProfileButton = ({ user, setShowModal, setLogin }) => {
               setShowModal(true);
             }}>Log in</button>
           </li>
-          {/* <li>
-            <hr />
-          </li> */}
-          {/* <li className="dropdown-text">
-            Host your home
-          </li>
-          <li className="dropdown-text">
-            Host an experience
-          </li>
-          <li className="dropdown-text">
-            Help
-          </li> */}
         </div>
-        </ul>
+        </div>
         )}
       </>
     );
